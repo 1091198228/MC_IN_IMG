758059348c700dbf9c1f867ed112f2c5dcfac155
daichenglong <1091198228@qq.com>
2020-04-11 17:48:00 +0800

    添加注释

--- MC_IN_IMG.py
+++ MC_IN_IMG.py
@@ -13,6 +13,7 @@ imgdir="5.jpg"
 
 #我的世界函数文件路径
 mcfunction=r"D:\GAME\MC\.minecraft\versions\1.15.2-forge-31.1.18\saves\test\datapacks\mytest\data\test\functions\test1.mcfunction"
+
 #目标生成区域左上角坐标
 xyz = [244,255,160]
 
@@ -22,20 +23,25 @@ def openimg(imgdir,bl):
     imge=cv2.resize(imge,(int(imge.shape[1]/bl),int(imge.shape[0]/bl)),interpolation=cv2.INTER_AREA)
     return imge
 
-#minecraft:glowstone 255  荧石
-#minecraft:black_wool <200 黑色羊毛
-#minecraft:stone            石头
+#minecraft:glowstone    荧石
+#minecraft:black_wool   黑色羊毛
+#minecraft:stone        石头
+#如果想显示更多颜色请添加命令生成规则
 
 def echocode(xyz,vlue,row,col):
     #传入坐标、像素值、y轴偏移、x轴偏移
+    #根据传入的像素值来判断生成什么颜色的方块
     x=xyz[0]
     y=xyz[1]
     z=xyz[2]
     if vlue > 200:
+        #荧石
         return("fill {} {} {} {} {} {} minecraft:glowstone\n".format(x+col,y-row,z,x+col,y-row,z))
     elif vlue>20:
+        #黑色羊毛
         return("fill {} {} {} {} {} {} minecraft:black_wool\n".format(x+col,y-row,z,x+col,y-row,z))
     else:
+        #石头
         return("fill {} {} {} {} {} {} minecraft:stone\n".format(x+col,y-row,z,x+col,y-row,z))
 
 def writetxt(mcfunction,xyz,imge):
@@ -47,9 +53,11 @@ def writetxt(mcfunction,xyz,imge):
             file.write(echocode(xyz,imge[row][col], row, col))        
     file.close()
 
+
 imge=openimg(imgdir,3)
 writetxt(mcfunction,xyz,imge)
 
+#预览图片
 cv2.namedWindow("Faces")
 cv2.imshow("Faces", imge)
 cv2.waitKey(0)        
